034aa34564349ba2ec073f61f57f1512e4173702
tkhe <violin1229@163.com>
2024-08-17 01:42:57 +0800

    support COCO evaluation

--- nora/evaluation/__init__.py
+++ nora/evaluation/__init__.py
@@ -1,4 +1,7 @@
+from .coco_evaluation import *
 from .evaluator import *
+from .panoptic_evaluation import *
 from .pascal_voc_evaluation import *
+from .rotated_coco_evaluation import *
 from .sem_seg_evaluation import *
 from .testing import *
