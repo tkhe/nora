3660bd808e910891376e68c8e8e0ce3ad42eec19
tkhe <violin1229@163.com>
2024-08-17 15:02:06 +0800

    add PPM

--- nora/layers/__init__.py
+++ nora/layers/__init__.py
@@ -5,6 +5,7 @@ from .mask_ops import *
 from .misc import *
 from .nms import *
 from .norm import *
+from .ppm import *
 from .roi_align import *
 from .shape_spec import *
 from .transformer import *
