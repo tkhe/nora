174c3344be5937f524fd630b990243e4fb4df682
tkhe <violin1229@163.com>
2024-10-19 09:57:23 +0800

    add constants

---
+++ configs/common/data/constants.py
@@ -0,0 +1,6 @@
+constants = dict(
+    imagenet_rgb256_mean=[123.675, 116.28, 103.53],
+    imagenet_rgb256_std=[58.395, 57.12, 57.375],
+    imagenet_bgr256_mean=[103.530, 116.280, 123.675],
+    imagenet_bgr256_std=[57.375, 57.120, 58.395],
+)
