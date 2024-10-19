2935b1aad9138ee2098d34367d0275565efbbca9
tkhe <violin1229@163.com>
2024-10-19 09:58:11 +0800

    add train config

---
+++ configs/common/train.py
@@ -0,0 +1,19 @@
+train = dict(
+    output_dir="./output",
+    init_checkpoint="",
+    max_iter=90000,
+    amp=dict(enable=False),
+    ddp=dict(
+        broadcast_buffers=False,
+        find_unused_parameters=False,
+        fp16_compression=False,
+    ),
+    checkpointer=dict(
+        period=5000,
+        max_to_keep=100,
+    ),
+    eval_period=5000,
+    log_period=10,
+    device="cuda",
+    precise_bn=dict(enable=False),
+)
